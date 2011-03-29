05005dba999515c9577a562f3aeeda06fa340581
edivaldosilvalmeida@gmail.com <edivaldosilvalmeida@gmail.com@24e8834e-f625-17c1-b3de-64d6bc762753>
2011-03-29 17:18:30 +0000

    trabalho compiladores

---
+++ trunk/sablecc-3.2/destination/bin/sablecc.bat
@@ -0,0 +1,10 @@
+rem * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+rem * This file is part of SableCC.                             *
+rem * See the file "LICENSE" for copyright information and the  *
+rem * terms and conditions for copying, distribution and        *
+rem * modification of SableCC.                                  *
+rem * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+rem
+rem IMPORTANT: Put the absolute path to sablecc.jar below.
+rem
+java -jar /home/ec2007/ra073988/sablecc-3.2/destination\lib\sablecc.jar %1 %2 %3 %4 %5 %6 %7 %8 %9
---
+++ trunk/sablecc-3.2/lexer/minijava/Compiler.java
@@ -0,0 +1,28 @@
+package minijava;
+//import minijava.parser.*;
+import minijava.lexer.*;
+import minijava.node.*;
+import java.io.*;
+
+public class Compiler{
+    
+    public static void main(String[] arguments){
+	try{
+	    
+	    // Create a lexer instance.
+	    Lexer l = new Lexer(new PushbackReader
+		(new InputStreamReader(System.in), 1024));
+		
+	    Token t = l.next();
+	    while (!t.getText().equals("")){ 
+		System.out.print(t.toString());
+		t = l.next(); 
+	    }
+	}
+	catch(Exception e){
+	    System.out.println(e.getMessage());
+	}
+	
+    }
+}
+
